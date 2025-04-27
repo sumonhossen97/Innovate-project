7256f21612dc18ed28d87cc91af2e72b4620135b
sumonhossen97 <sumonhossen0277@gmail.com>
2025-04-27 22:55:16 +0600

    some components

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,13 @@ import List from './Components/List'
 import Button from './Components/Button'
 import Heading from './Components/Heading'
 import SubHeading from './Components/SubHeading'
+import Image from './Components/Image'
+import BannerImage from './assets/banner.png'
 
 const App = () => {
   return (
     <div>
-      <Heading text="This is Sumon"/>
-      <SubHeading className='w-[393px]' text="We are now a team of strategists, engineers, designers, and marketers who both use and develop technology for a variety of client needs."/>
+      <Image src={BannerImage} alt="kahlshgak"/>
     </div>
   )
 }
